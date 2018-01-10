7f9cf8bec65790b33561460b8187cfcaaa17b24a
saimanasagit <35286790+saimanasagit@users.noreply.github.com>
2018-01-09 22:47:16 -0800

    new line added

--- src/gitpkg/SampleGIT.java
+++ src/gitpkg/SampleGIT.java
@@ -5,7 +5,7 @@ public class SampleGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi git");
-
+		System.out.println("hello git");
 	}
 
 }
